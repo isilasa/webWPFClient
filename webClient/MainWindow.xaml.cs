88e49b40f984abe809198205a3b9197697db6794
isilasa <isilasaquitall@gmail.com>
2019-12-23 16:06:39 +0300

    Добавьте файлы проекта.

---
+++ webClient/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace webClient
+{
+    class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static ClientWebSocket webClient;
+        Uri uri = new Uri("ws://localhost:8765");
+        CancellationToken token = new CancellationToken();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void buttonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            webClient = new ClientWebSocket();
+            webClient.ConnectAsync(uri,token);
+            
+        }
+
+        private void buttonSend_Click(object sender, RoutedEventArgs e)
+        {
+            Person Tom = new Person { name = "Tom", age = 35 };
+            byte[] jsonData = JsonSerializer.SerializeToUtf8Bytes<Person>(Tom);
+            ArraySegment<byte> dataForSend = new ArraySegment<byte>(jsonData);
+
+            webClient.SendAsync(dataForSend, 0, true, token);
+
+            
+        }
+    }
+}
