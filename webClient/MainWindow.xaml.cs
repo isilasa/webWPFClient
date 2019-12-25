3b6abb12800eb42026e009c7ebb1be8c60d1c838
isilasa <isilasaquitall@gmail.com>
2019-12-25 11:30:26 +0300

    v2

--- webClient/MainWindow.xaml.cs
+++ webClient/MainWindow.xaml.cs
@@ -1,22 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net.WebSockets;
 using System.Threading;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Runtime.Serialization;
+using System.ServiceModel.Channels;
+using System.IO;
 
 namespace webClient
 {
@@ -38,8 +26,22 @@ namespace webClient
         public MainWindow()
         {
             InitializeComponent();
-
         }
+        public virtual Message ReadMessage(ArraySegment<byte> buffer, BufferManager bufferManager, string contentType)
+        {
+            byte[] msgContents = new byte[buffer.Count];
+            Array.Copy(buffer.Array, buffer.Offset, msgContents, 0, msgContents.Length);
+            bufferManager.ReturnBuffer(buffer.Array);
+
+            MemoryStream stream = new MemoryStream(msgContents);
+            return ReadMessage(stream, int.MaxValue);
+        }
+
+        private Message ReadMessage(MemoryStream stream, int maxValue)
+        {
+            throw new NotImplementedException();
+        }
+
 
         private void buttonConnect_Click(object sender, RoutedEventArgs e)
         {   
@@ -50,25 +52,21 @@ namespace webClient
 
         private void buttonSend_Click(object sender, RoutedEventArgs e)
         {
-            
             Person Tom = new Person { name = "Tom", age = 35 };
             byte[] jsonDataForSend = JsonSerializer.SerializeToUtf8Bytes<Person>(Tom);
             string jsonDataForClient = JsonSerializer.Serialize<Person>(Tom);
             ArraySegment<byte> dataForSend = new ArraySegment<byte>(jsonDataForSend);
+            var buffer = new ArraySegment<byte>(new byte[1024]);
 
-            webClient.SendAsync(dataForSend, 0, true, token);
+
+            webClient.SendAsync(dataForSend, WebSocketMessageType.Binary, true, token);
             requestToServerList.Items.Add(jsonDataForClient);
 
+            webClient.ReceiveAsync(buffer, token);
+            //string recvDataForRead = JsonSerializer.Deserialize(buffer, returnType, null);
+            BufferManager buff = null;
+            responceFromServerList.Items.Add(ReadMessage(buffer,  buff, null));
 
-            ArraySegment<byte> dataForRecive = new ArraySegment<byte>(jsonDataForSend);
-            webClient.ReceiveAsync(dataForRecive, token);
-
-            responceFromServerList.Items.Add(dataForRecive);
-
-                   
-            
-
-            
         }
     }
 }
