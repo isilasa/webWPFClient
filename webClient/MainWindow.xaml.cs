9988a78f966ac7fa86d50d2e7fbc8dd5f43130cb
isilasa <isilasaquitall@gmail.com>
2019-12-26 13:45:16 +0300

    add to list box

--- webClient/MainWindow.xaml.cs
+++ webClient/MainWindow.xaml.cs
@@ -1,22 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net.WebSockets;
 using System.Threading;
 using System.Text.Json;
-using System.ServiceModel.Channels;
-using System.IO;
+using System.Text;
 
 namespace webClient
 {
@@ -33,50 +20,28 @@ namespace webClient
         private static ClientWebSocket webClient;
         Uri uri = new Uri("ws://localhost:8765");
         CancellationToken token = new CancellationToken();
-
-
         public MainWindow()
         {
             InitializeComponent();
         }
-        public virtual Message ReadMessage(ArraySegment<byte> buffer, BufferManager bufferManager, string contentType)
-        {
-            byte[] msgContents = new byte[buffer.Count];
-            Array.Copy(buffer.Array, buffer.Offset, msgContents, 0, msgContents.Length);
-            bufferManager.ReturnBuffer(buffer.Array);
-
-            MemoryStream stream = new MemoryStream(msgContents);
-            return ReadMessage(stream, int.MaxValue);
-        }
-
-        private Message ReadMessage(MemoryStream stream, int maxValue)
-        {
-            throw new NotImplementedException();
-        }
-
-
         private void buttonConnect_Click(object sender, RoutedEventArgs e)
         {
             webClient = new ClientWebSocket();
             webClient.ConnectAsync(uri,token);
-            
         }
-
         private void buttonSend_Click(object sender, RoutedEventArgs e)
         {
             Person Tom = new Person { name = "Tom", age = 35 };
             byte[] jsonDataForSend = JsonSerializer.SerializeToUtf8Bytes<Person>(Tom);
-            string jsonDataForClient = JsonSerializer.Serialize<Person>(Tom);
             ArraySegment<byte> dataForSend = new ArraySegment<byte>(jsonDataForSend);
-            var buffer = new ArraySegment<byte>(new byte[1024]);
+            ArraySegment<byte> bufferForRecv = new ArraySegment<byte>(new byte[1024]);
 
 
             webClient.SendAsync(dataForSend, 0, true, token);
+            requestToServerList.Items.Add(Encoding.UTF8.GetString(dataForSend.Array));
 
-            webClient.ReceiveAsync(buffer, token);
-            //string recvDataForRead = JsonSerializer.Deserialize(buffer, returnType, null);
-            BufferManager buff = null;
-            responceFromServerList.Items.Add(ReadMessage(buffer,  buff, null));
+            webClient.ReceiveAsync(bufferForRecv, token);
+            responceFromServerList.Items.Add(Encoding.UTF8.GetString(bufferForRecv.Array));
 
         }
     }
